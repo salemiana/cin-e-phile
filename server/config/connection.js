10255cc444d03f7ad885935798441e8e84ddbf42
valcomp <valerie.aleida.lopez@gmail.com>
2022-08-24 06:33:09 -0500

    deploying attempt 2

--- server/config/connection.js
+++ server/config/connection.js
@@ -2,7 +2,7 @@ const mongoose =require('mongoose');
 require('dotenv').config();
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/cineview',
+  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/cineviewdata',
   {
     useNewUrlParser: true, 
     useUnifiedTopology: true,
